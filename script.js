bf396957d575f2cb3531a75a1305fe0e152498a9
KlaraK00 <klara.kneisl.2000@gmail.com>
2022-11-16 18:39:43 +0100

    add winner lines

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let fields = [];
 
 let currentShape = "cross";
 
+let directionOfLine = "";
+
 function fillShape(id) {
     fields[id] = currentShape;
     isItCircleOrCross(id);
@@ -12,21 +14,35 @@ function fillShape(id) {
 function isItCircleOrCross(id) {
     if (currentShape == "cross") {
         fillCross(id);
+        changeToPlayer1();
     } else {
         fillCircle(id); 
+        changeToPlayer2();
     }
 }
 
 function fillCross(id) {
+    document.getElementById(`div${id}`).parentElement.classList.add('notClickable');
     document.getElementById(`cross${id}`).classList.remove('d-none');
     document.getElementById(`circle${id}`).classList.add('d-none');
     currentShape = "circle";
 }
 
+function changeToPlayer1() {
+    document.getElementById('player1').classList.remove('playerInactive');
+    document.getElementById('player2').classList.add('playerInactive');
+}
+
 function fillCircle(id) {
+    document.getElementById(`div${id}`).parentElement.classList.add('notClickable');
     document.getElementById(`cross${id}`).classList.add('d-none');
     document.getElementById(`circle${id}`).classList.remove('d-none');
     currentShape = "cross";
+}
+
+function changeToPlayer2() {
+    document.getElementById('player2').classList.remove('playerInactive');
+    document.getElementById('player1').classList.add('playerInactive');
 }
 
 function checkWinner() {
@@ -34,19 +50,55 @@ function checkWinner() {
     for (let i = 0; i < 9; i+=3) {
         if (fields[i] == fields[i+1] && fields[i+1] == fields[i+2] && fields[i]) {
             console.log(fields[i] + ' du hast gewonnen!');
+            directionOfLine = 'horizotal';
+            addHorizontalWinnerLine(i);
         } 
     }
-//vertikal
+//vertical
     for (let i = 0; i < 3; i++) {
         if (fields[i] == fields[i+3] && fields[i+3] == fields[i+6] && fields[i]) {
             console.log(fields[i] + ' du hast gewonnen!');
+            directionOfLine = 'vertical';
+            addVerticalWinnerLine(i);
         } 
     }
 //diagonal
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         console.log(fields[0] + ' du hast gewonnen!');
+        directionOfLine = 'diagonal1';
+        addDiagonalWinnerLine();
     }
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         console.log(fields[2] + ' du hast gewonnen!');
+        directionOfLine = 'diagonal2';
+        addDiagonalWinnerLine();
+    }
+}
+
+function addHorizontalWinnerLine(i) {
+    if (i == 0 &&  directionOfLine == 'horizotal') {
+        document.getElementById('line1').classList.add('scale1');
+    } else if (i == 3 && directionOfLine == 'horizotal') {
+        document.getElementById('line2').classList.add('scale1');
+    } else {
+        document.getElementById('line3').classList.add('scale1');
+    }   //i==6
+}
+
+function addVerticalWinnerLine(i) {
+    if (i == 0 &&  directionOfLine == 'vertical') {
+        document.getElementById('line4').classList.add('scale2');
+    } else if (i == 1 && directionOfLine == 'vertical') {
+        document.getElementById('line5').classList.add('scale2');
+    } else {
+        document.getElementById('line6').classList.add('scale2');
+    }   //i==2
+}
+
+function addDiagonalWinnerLine() {
+    if (directionOfLine == 'diagonal1') {
+        document.getElementById('line7').classList.add('scale3')
+    } else {
+        document.getElementById('line8').classList.add('scale4')
     }
 }
